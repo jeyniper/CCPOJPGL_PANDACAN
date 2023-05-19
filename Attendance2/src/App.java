f545d5e1276df4c88ce8d76c8361805026daf66b
Jennifer Pandacan <119916280+jeyniper@users.noreply.github.com>
2023-05-19 11:33:42 +0800

    Add files via upload

---
+++ Attendance2/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+
+    Hero1 hero = new Hero1();
+      System.out.println(hero.Name);
+      System.out.println(hero.Roles);
+      System.out.println("Attributtes: " + hero.Attributes);
+      System.out.println("Weakness: " + hero.Weakness);
+      
+      
+      hero.hero1action();
+      
+      hero.hero1action();
+      hero.Game();
+      
+   }
+}
---
+++ Attendance2/src/Hero.java
@@ -0,0 +1,8 @@
+public class Hero {
+	String Name = "Pharsa";
+	String Roles = "Mage";
+	
+	void Game() {
+		System.out.println("Locked Hero.");
+	}
+	}
